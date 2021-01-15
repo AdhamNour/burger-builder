8098bcc9180bacd2c1c0d28a23933b9170c5529d
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-01-15 18:52:11 +0200

    finished video 15 from module 8

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -10,7 +10,7 @@ const burger = (props) => {
         return arr.concat(el);
     });
 
-    if (transformedIngredient.length == 0) {
+    if (transformedIngredient.length === 0) {
         transformedIngredient = "Please add some ingredient"
     }
 
