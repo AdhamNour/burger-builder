8c48a2cf57e0693f2fb6450bd5a0161fe581cbee
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-01-17 20:39:50 +0200

    finished video 31 from module 8

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,12 +8,14 @@ import classes from './SideDrawer.module.css'
 const sideDrawer = () => {
     return ( 
         <div className = {classes.SideDrawer}>
-            <Logo />
+            <div className = {classes.Logo}>
+                <Logo />
+            </div>
             <nav>
                 <NavigationItems />
             </nav>
         </div>
-     );
+    );
 }
 
 export default sideDrawer;
