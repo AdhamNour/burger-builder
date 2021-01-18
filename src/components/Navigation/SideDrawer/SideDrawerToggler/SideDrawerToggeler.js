cb9cfd904305177da5f9452ecca0076fff5d9981
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-01-18 12:43:48 +0200

    Finished video 35 from module 8

--- src/components/Navigation/SideDrawer/SideDrawerToggler/SideDrawerToggeler.js
+++ src/components/Navigation/SideDrawer/SideDrawerToggler/SideDrawerToggeler.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import classes from './SideDrawer.module.css'
+
 const sideDrawerToggler = (props) => {
     return ( 
-        <div onClick={props.clicked}>Menu</div>
+        <div onClick={props.clicked} className={classes.DrawerToggle} >
+            <div></div>
+            <div></div>
+            <div></div>            
+        </div>
     );
 }
  
