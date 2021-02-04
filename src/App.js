426d60b8d94cf4df729352c2c978e24e05201b14
AdhamNour <adhamnourelwaffaa@gmail.com>
2021-02-04 06:25:56 -0500

    finished video 3 from module 12

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 function App() {
   return (
     <div >
-    <Layout>
-      <BurgerBuilder />
-      <Checkout />
-    </Layout>
+      <Layout>
+        <Switch>
+          <Route path='/chechout' component={Checkout} />
+          <Route path='/' exact component={BurgerBuilder} />
+        </Switch>
+      </Layout>
     </div>
   );
 }
 
 export default App;
- 
