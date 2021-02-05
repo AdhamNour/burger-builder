ef17493e8d360d9bf32619b12bb01b29bf493e12
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-02-05 13:27:28 +0200

    My Touch added

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import burgerLogo from '../../assets/images/burger-logo.png'
+import burgerLogo from "../../assets/images/burger-logo.png";
 
-import classes from './Logo.module.css'
+import classes from "./Logo.module.css";
 
 const Logo = (props) => {
-    return ( 
-        <div className = {classes.Logo}>
-            <img src={burgerLogo} alt ="My Burger" />
-        </div>
-     );
-}
- 
+  return (
+    <div className={classes.Logo}>
+      <Link to="/">
+        <img src={burgerLogo} alt="My Burger" />
+      </Link>
+    </div>
+  );
+};
+
 export default Logo;
