2b2e2128c41ec3c2a3a799274183d61a206564d6
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-02-05 14:17:56 +0200

    finished module 12

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React from "react";
 
-
-import classes from './Order.module.css'
+import classes from "./Order.module.css";
 
 const Order = (props) => {
-    const ingredients =[];
-    for(let ingredientName in props.ingredients ){
-        ingredients.push({name: ingredientName,amount:props.ingredients[ingredientName]})
-    }
-    const ingredientsOutput = ingredients.map(ig => {
-        return <span key={ig.name} style={{textTransform:'capitalize',display:'inline-block',margin:'0 8px',border:'1px solid #ccc',padding:'5px'}} ><strong>{ig.name}</strong>:<strong>{ig.amount}</strong></span>
-    })
-    return ( 
-        <div className={classes.Order}>
-            <p>ingredients are {ingredientsOutput}</p>
-        </div>
-     );
-}
- 
+  const ingredients = [];
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName],
+    });
+  }
+  const ingredientsOutput = ingredients.map((ig) => {
+    return (
+      <span
+        key={ig.name}
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid #ccc",
+          padding: "5px",
+        }}
+      >
+        <strong>{ig.name}</strong>:<strong>{ig.amount}</strong>
+      </span>
+    );
+  });
+  return (
+    <div className={classes.Order}>
+      <p>ingredients are {ingredientsOutput}</p>
+    </div>
+  );
+};
+
 export default Order;
