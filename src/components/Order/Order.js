441f6b7a6cd8911ae861745dbc1025a695a21f0a
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-02-05 13:09:52 +0200

    finished video 9 from module 12

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Order = () => {
+    return ( 
+        <div>
+            <p>ingredients are salad 1</p>
+            <p>price:5USDs</p>
+        </div>
+     );
+}
+ 
+export default Order;
